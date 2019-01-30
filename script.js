53520031b68f70263b07e0050ac62d21fdd25ba9
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-30 10:11:53 -0600

    Create operate() function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//These are the formulas that will run the calculations
 function add(a, b) {
   return a + b;
 }
@@ -11,10 +12,31 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  //If the user attempts to divide by zero, they'll get an error message
   if (b === 0) {
     return 'Can\'t divide by zero!!';
 
   } else {
     return a / b;
+  }
+}
+
+//This function determines which operation to run based on the given parameters
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+
+    case '-':
+      return subtract(num1, num2);
+
+    case '*':
+      return multiply(num1, num2);
+
+    case '/':
+      return divide(num1, num2);
+
+    default:
+      return 'Sorry, ' + operator + ' isn\'t a valid operator';
   }
 }

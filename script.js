0d85b790eb33a07f03965f6df116caa073c61db9
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-30 14:04:43 -0600

    Create createButtons() function, modify some class names

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const buttonContainer = document.querySelector('.button-container');
+
 //These are the formulas that will run the calculations=======================//
 function add(a, b) {
   return a + b;
@@ -39,5 +41,21 @@ function operate(num1, operator, num2) {
 
     default:
       return 'Sorry, ' + operator + ' isn\'t a valid operator';
+  }
+}
+
+//Generates the calculator buttons============================================//
+function createButtons() {
+  const buttonLabels = [['1', 'one'], ['2', 'two'], ['3', 'three'], ['4', 'four'], ['5', 'five'], ['6', 'six'], ['7', 'seven'], ['8', 'eight'], ['9', 'nine'], ['C', 'clear'], ['0', 'zero'], ['.', 'decimal'], ['+', 'add'], ['-', 'subtract'], ['x', 'multiply'], ['÷', 'divide'], ['=', 'equals']];
+
+  for(let i = 0; i < buttonLabels.length; i++) {
+    const calcButton = document.createElement('button');
+
+    calcButton.setAttribute('id', 'button-' + buttonLabels[i][1]);
+    calcButton.setAttribute('class', 'calc-button');
+    
+    calcButton.textContent = buttonLabels[i][0];
+
+    buttonContainer.appendChild(calcButton);
   }
 }

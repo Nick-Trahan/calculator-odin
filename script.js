99584604555b2337363fc9da760f458cf3dd14ae
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-30 15:56:19 -0600

    Add normalize.css, begin creating grid layout

--- script.js
+++ script.js
@@ -48,13 +48,13 @@ function operate(num1, operator, num2) {
 
 //Generates the calculator buttons============================================//
 function createButtons() {
-  const buttonLabels = [['1', 'one'], ['2', 'two'], ['3', 'three'], ['4', 'four'], ['5', 'five'], ['6', 'six'], ['7', 'seven'], ['8', 'eight'], ['9', 'nine'], ['C', 'clear'], ['0', 'zero'], ['.', 'decimal'], ['+', 'add'], ['-', 'subtract'], ['x', 'multiply'], ['÷', 'divide'], ['=', 'equals']];
+  const buttonLabels = [['1', 'one', 'row1'], ['2', 'two', 'row1'], ['3', 'three', 'row1'], ['4', 'four', 'row1'], ['5', 'five', 'row2'], ['6', 'six', 'row2'], ['7', 'seven', 'row2'], ['8', 'eight', 'row2'], ['9', 'nine', 'row3'], ['C', 'clear', 'row3'], ['0', 'zero', 'row3'], ['.', 'decimal', 'row3'], ['+', 'add', 'row4'], ['-', 'subtract', 'row4'], ['x', 'multiply', 'row4'], ['÷', 'divide', 'row4'], ['=', 'equals', 'row5']];
 
   for(let i = 0; i < buttonLabels.length; i++) {
     const calcButton = document.createElement('button');
 
     calcButton.setAttribute('id', 'button-' + buttonLabels[i][1]);
-    calcButton.setAttribute('class', 'calc-button');
+    calcButton.setAttribute('class', 'calc-button ' + buttonLabels[i][2]);
     
     calcButton.textContent = buttonLabels[i][0];
 

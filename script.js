ba837c8bbde2cb7b6487962fbdb87092b4196c1c
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-30 09:25:40 -0600

    Modify divide() so the user can't divide by zero

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ function subtract(a, b) {
   return a - b;
 }
 
-function multiply(array) {
-  return array.reduce((a, b) => a * b);
+function multiply(a, b) {
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b;
+  if (b === 0) {
+    return 'Can\'t divide by zero!!';
+
+  } else {
+    return a / b;
+  }
 }

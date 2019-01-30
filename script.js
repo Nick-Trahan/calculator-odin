b4a836265a7b79b69dcd4551c3a37d8acd8b756e
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-30 08:54:53 -0600

    Create script.js and write first calculator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(array) {
+  return array.reduce((a, b) => a * b);
+}
+
+function divide(a, b) {
+  return a / b;
+}
